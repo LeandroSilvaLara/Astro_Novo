30ec70375f33308909e1ebaa534b9eb2f9baf91a
Leandro <leandrodasilvalara@yahoo.com.br>
2021-12-02 23:10:56 -0300

    :construction: Criando um service usando o Retrofit

--- app/src/main/java/br/com/leandro/astro/data/di/DataModule.kt
+++ app/src/main/java/br/com/leandro/astro/data/di/DataModule.kt
@@ -1,11 +1,12 @@
 package br.com.leandro.astro.data.di
 
-import br.com.leandro.astro.data.repository.MockAPIService
 import br.com.leandro.astro.data.repository.PostRepository
 import br.com.leandro.astro.data.repository.PostRepositoryImpl
+import br.com.leandro.astro.data.services.SpaceFlightNewsService
 import org.koin.core.context.loadKoinModules
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 /**
  * Esse Object organiza as dependências da camada Data
@@ -15,13 +16,26 @@ import org.koin.dsl.module
 
 object DataModule {
 
+    private const val BASE_URL = "https://api.spaceflightnewapi.net/v3/"
+
     fun load() {
         loadKoinModules(postsModule())
     }
 
+    /**
+     * Criar uma instância de PostRepositoryImpl usando o service
+     * mockado.
+     */
     private fun postsModule() : Module {
         return module {
-            single<PostRepository> { PostRepositoryImpl( MockAPIService ) }
+            single<PostRepository> { PostRepositoryImpl( createService() ) }
         }
+    }
+
+    private inline fun createService() : SpaceFlightNewsService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .build()
+            .create(SpaceFlightNewsService::class.java)
     }
 }
